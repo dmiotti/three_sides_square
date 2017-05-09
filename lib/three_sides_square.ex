428c6b111b691e0ab7ca374f2faf8a576e1276bf
David Miotti <david.miotti@wopata.com>
2017-05-09 12:15:43 +0200

    Initial Elixir app

---
+++ lib/three_sides_square.ex
@@ -0,0 +1,9 @@
+defmodule ThreeSidesSquare do
+  def load(file) do
+  end
+
+  def read_line(line) do
+  end
+
+  def is_triangle_valid({d1, d2, d3}), do: d3 > d1 + d2
+end
---
+++ test/three_sides_square_test.exs
@@ -0,0 +1,8 @@
+defmodule ThreeSidesSquareTest do
+  use ExUnit.Case
+  doctest ThreeSidesSquare
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
